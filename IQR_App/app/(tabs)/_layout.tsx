1dc3a27e633f8c954ee8594d08a5790f7ddbd83c
Stefan Fourie <stefanfourie001@gmail.com>
2025-12-10 22:12:14 +0200

    Tested Some Shii

--- IQR_App/app/(tabs)/_layout.tsx
+++ IQR_App/app/(tabs)/_layout.tsx
@@ -30,6 +30,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="reports"
+        options={{
+          title: 'Reports',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />,
+        }}
+      />
     </Tabs>
+    
   );
 }
---
+++ IQR_App/app/(tabs)/reports.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View }  from "react-native";
+
+export default function ReportsScreen() {
+  return (
+    <View >
+      <Text >Reports Screen</Text>
+    </View>
+  );
+
+
+}
